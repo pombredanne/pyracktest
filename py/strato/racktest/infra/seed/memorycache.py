8364dd6959928c6250d6c327c4b0c747ef96b338
Alexander Solganik <sasha@stratoscale.com>
2015-10-01 19:27:41 +0300

    memory cache implementation.
    This  cache uses shmem and performs well when running concurrently seeds in single test.
    The reason for shmem is because concurrently is a multiprocess and not multithread.
    This cacher does not handle seed dependency as it assumes that code does not change during the test.
    This issue must be handled for DEVELOPER_MODE in systemtests where code actually do change during test

---
+++ py/strato/racktest/infra/seed/memorycache.py
@@ -0,0 +1,29 @@
+from contextlib import contextmanager
+import multiprocessing
+import logging
+
+
+class MemoryCache(object):
+
+    def __init__(self):
+        self._lock = multiprocessing.Lock()
+        self._manager = multiprocessing.Manager()
+        self._cache = self._manager.dict()
+
+    @contextmanager
+    def lock(self, key):
+        self._lock.acquire(timeout=60)
+        try:
+            yield
+        finally:
+            self._lock.release()
+
+    def get(self, key):
+        return self._cache.get(key, None)
+
+    def install(self, seedKey, seedEntry):
+        logging.debug('Adding seed for key %(key)s to cache', dict(key=seedKey))
+        self._cache[seedKey] = seedEntry['code']
+
+from strato.racktest.infra.seed import cacheregistry
+cacheregistry.register('memory', MemoryCache.__init__)
