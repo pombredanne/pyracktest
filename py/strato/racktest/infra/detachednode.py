f0e06d41c4fe7e9cf6831f86b55f122d9279f22e
Igal Tsoiref <igal.tsoireg@stratoscale.com>
2016-07-10 16:49:36 +0300

    Change DetachedNode class : 	1. adding network info 	2. change base class from api.Node to rackattack-api Node

--- py/strato/racktest/infra/detachednode.py
+++ py/strato/racktest/infra/detachednode.py
@@ -1,14 +1,18 @@
 from rackattack import api
+from rackattack import clientfactory
+from rackattack.tcp import node
 
 
-class DetachedNode(api.Node):
-    def __init__(self, username, password, hostname, port, ipAddress, nodeId):
+class DetachedNode(node.Node):
+    def __init__(self, username, password, hostname, port, ipAddress, nodeId, info):
         self._username = username
         self._password = password
         self._hostname = hostname
         self._port = port
         self._ipAddress = ipAddress
-        self._id = id
+        self._id = nodeId
+        self._info = info
+        self._ipcClient = clientfactory.factory()
 
     def rootSSHCredentials(self):
         return dict(hostname=self._hostname,
@@ -19,6 +23,3 @@ class DetachedNode(api.Node):
 
     def ipAddress(self):
         return self._ipAddress
-
-    def id(self):
-        return self._id
